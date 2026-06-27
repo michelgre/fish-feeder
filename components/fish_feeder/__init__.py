4211ab9656e0a1a94a25e4239879a9f6ae62cf28
michelgre <michelgre@gmail.com>
2026-06-27 16:57:08 +0200

    V0.2 - Composant configurable

--- components/fish_feeder/__init__.py
+++ components/fish_feeder/__init__.py
@@ -2,6 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import servo
 from esphome.const import CONF_ID
+from esphome import pins
 
 DEPENDENCIES = ["servo"]
 
@@ -9,11 +10,20 @@ fish_feeder_ns = cg.esphome_ns.namespace("fish_feeder")
 FishFeeder = fish_feeder_ns.class_("FishFeeder", cg.Component)
 
 CONF_SERVO = "servo"
+CONF_OPEN_ANGLE = "open_angle"
+CONF_CLOSE_ANGLE = "close_angle"
+CONF_OPEN_DURATION = "open_duration"
 
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(FishFeeder),
+
         cv.Required(CONF_SERVO): cv.use_id(servo.Servo),
+
+        cv.Optional(CONF_OPEN_ANGLE, default=90): cv.angle,
+        cv.Optional(CONF_CLOSE_ANGLE, default=0): cv.angle,
+
+        cv.Optional(CONF_OPEN_DURATION, default="300ms"): cv.positive_time_period_milliseconds,
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
@@ -24,3 +34,7 @@ async def to_code(config):
 
     servo_var = await cg.get_variable(config[CONF_SERVO])
     cg.add(var.set_servo(servo_var))
+
+    cg.add(var.set_open_angle(config[CONF_OPEN_ANGLE]))
+    cg.add(var.set_close_angle(config[CONF_CLOSE_ANGLE]))
+    cg.add(var.set_open_duration(config[CONF_OPEN_DURATION]))
--- components/fish_feeder/fish_feeder.h
+++ components/fish_feeder/fish_feeder.h
@@ -8,13 +8,22 @@ namespace fish_feeder {
 
 class FishFeeder : public Component {
  public:
-  void set_servo(servo::Servo *servo) { servo_ = servo; }
-
   void setup() override;
   void loop() override;
 
   void feed();
   void reset();
+
+  void set_servo(servo::Servo *servo) { servo_ = servo; }
+
+  void set_open_angle(float v) { open_angle_ = v; }
+  void set_close_angle(float v) { close_angle_ = v; }
+  void set_open_duration(uint32_t ms) { open_duration_ms_ = ms; }
+
+private:
+  float open_angle_{90};
+  float close_angle_{0};
+  uint32_t open_duration_ms_{300};
 
  protected:
   servo::Servo *servo_{nullptr};
